941ae29b0577b4672f880f71121e96998d9b78c9
Daniil <daniilkarp2003@mail.ru>
2025-01-19 22:44:36 +0300

    redis_monitor for graphana

--- gateway/src/main/java/com/example/configuration/RedisConfig.java
+++ gateway/src/main/java/com/example/configuration/RedisConfig.java
@@ -6,6 +6,7 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.redis.RedisConnectionFailureException;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnection;
--- kibana/init.sh
+++ kibana/init.sh
@@ -7,3 +7,4 @@ curl -X POST "http://localhost:5601/api/saved_objects/index-pattern/gateway-logs
              "timeFieldName": "@timestamp"
            }
          }'
+
